2c6e30b7d0b82ab8272e3284935ee1aadbc589cd
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-12 14:33:06 +0530

    add pop method

--- src/main/Stack.java
+++ src/main/Stack.java
@@ -31,8 +31,30 @@ public class Stack {
     private int size = 0;
     public void push(int data)
     {
+        Node newNode = new Node(data, this.top);
+        this.top = newNode;
+        this.size++;
+    }
+    public int pop()
+    {
+        int response = -1;
+        if(this.top!=null)
+        {
+            response=this.top.getData();
+            this.top=this.top.getNext();
+            this.size--;
+        }
+        else
+        {
+            System.out.println("Stack is Empty");
+        }
+        return response;
+    }
+
+    public static void main(String[] args) {
 
     }
+
 
 
 }
