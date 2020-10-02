9e65b1e0bc8670e0a04b857d81dbc52fc8a7bf18
aniket1418 <36506092+aniket1418@users.noreply.github.com>
2020-10-02 09:22:07 +0530

    Update Stack.java

--- src/main/Stack.java
+++ src/main/Stack.java
@@ -58,3 +58,39 @@ public class Stack {
 
 
 }
+
+
+Another Stack Pop method Solution
+
+
+// Java code to illustrate pop() 
+import java.util.*; 
+  
+public class StackDemo { 
+    public static void main(String args[]) 
+    { 
+        // Creating an empty Stack 
+        Stack<String> STACK = new Stack<String>(); 
+  
+        // Use add() method to add elements 
+        STACK.push("Welcome"); 
+        STACK.push("To"); 
+        STACK.push("Geeks"); 
+        STACK.push("For"); 
+        STACK.push("Geeks"); 
+  
+        // Displaying the Stack 
+        System.out.println("Initial Stack: " + STACK); 
+  
+        // Removing elements using pop() method 
+        System.out.println("Popped element: " +  
+                                         STACK.pop()); 
+        System.out.println("Popped element: " +  
+                                         STACK.pop()); 
+  
+        // Displaying the Stack after pop operation 
+        System.out.println("Stack after pop peration "
+                                             + STACK); 
+    } 
+} 
+
